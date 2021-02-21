8c31df972f136971c745f296692bcb6ad9d7e358
rgwang <303698450@qq.com>
2021-02-21 14:59:09 +0800

    switch statement

--- t2/main.cpp
+++ t2/main.cpp
@@ -1,14 +1,45 @@
- #include <iostream>
+#include <iostream>
 
 using namespace std;
 
 void limits(); // 输出各种整型所占的字节数，所能表示的最值
 
+void floatnum(); // 浮点类型
+
+void switchStatement(); // switch语句
+
 int main()
 {
 //    std::cout << "Hello, World!" << std::endl;
-    limits();
+//    floatnum();
+//    limits();
+    switchStatement();
     return 0;
+}
+
+void switchStatement()
+{
+    int vehicleClass;
+    double toll;
+    cout << "Enter vehicle class: ";
+    cin >> vehicleClass;
+
+    switch (vehicleClass) {
+        case 1:
+            cout << "Passenger car.";
+            toll = 0.5;
+            break;
+        case 2:
+            cout << "Bus.";
+            toll = 1.5;
+            break;
+        case 3:
+            cout << "Truck.";
+            toll = 2.;
+            break;
+        default:
+            cout << "Unknown vehicle class!";
+    }
 }
 
 void limits()
@@ -32,4 +63,20 @@ void limits()
 
     cout << "Minimum int value = " << INT_MIN << endl;
     cout << "Bits per byte = " << CHAR_BIT << endl;
+}
+
+void floatnum()
+{
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+    float tub = 10.0/3.0;
+    double mint = 10.0 / 3.0;
+    const float million = 1.0e6;
+
+    cout << "tub = " << tub;
+    cout << ", a million tubs = " << million * tub;
+    cout << ",\nand ten million tubs = ";
+    cout << 10 * million * tub << endl;
+
+    cout << "mint = " << mint << " and a million mints = ";
+    cout << million * mint << endl;
 }
